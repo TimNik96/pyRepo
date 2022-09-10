73a3284970bb3a646dec71d210d9f28472f6d03d
ntimotic <timotic.nikola.27@gmail.com>
2022-09-10 18:39:16 +0200

    finish first class

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pyRepo
 pythonCourse
+
+# CAS_1
+
+Na prvom predavanju se pricalo o opstim stvarima vezano za jezik. Obradjen je deo kreiranja promenljive, kao i prikupljanje podataka od korisnika.
+Takodje je demonstrirano kako se kroz pseudo kod ili algoritamski moze predstaviti resenje odredjenog problema.
---
+++ cas_1.py
@@ -0,0 +1,17 @@
+# - Napisati program koji ce za unos imena, prezimena i broja godina kreirati sledeci ispis:
+#   ime.prezime.godine@gmail.com
+
+# ime = input("Unesite ime: ")
+# prezime = input("Unesite prezime: ")
+# godine = input ("Unesite godine: ")
+
+# print(ime + "." + prezime + "." + godine + "@gmail.com")
+
+# - Napisati program koji ce za unos imena i prezimena, kao i godina, kreirati sledeci ispis:
+#   Ja sam Nikola Timotic i imam 27 godina.
+
+ime = input("Unesite ime: ")
+prezime = input("Unesite prezime: ")
+godine = eval(input ("Unesite godine: "))
+
+print("Ja sam " + ime + " " + prezime + " i imam", godine, "godina.")
