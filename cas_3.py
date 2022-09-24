336aa327a7765f9137122a2b9dccb595ae17345d
ntimotic <timotic.nikola.27@gmail.com>
2022-09-24 17:28:11 +0200

    add imports and  loops

---
+++ cas_3.py
@@ -0,0 +1,72 @@
+# for i in range(10):
+#     print("Pozdrav")
+
+# -Napisati program koji racuna stepen unetog broja
+
+    # osnova = eval(input("Uneti osnovu za stepenovanje: "))
+    # stepen = eval(input("Uneti eksponent za stepenovanje: "))
+    # rezultat = 1
+
+    # for i in range(stepen):
+    #     rezultat = rezultat * osnova
+
+    # print(rezultat)
+
+# -Napisati program koji vrsi odbrojavanje unazad
+
+# for i in range(10, -1, -1):
+#     print(i, end=' ')
+
+# -Napisati program koji prevodi Celzijuse u Farenhajte
+
+    # temp = 0
+
+    # while temp != -1000:
+    #     temp = eval(input("Unesite temperaturu za konverziju: "))
+    #     if temp == -1000:
+    #         print("Iskljucivanje programa...")
+    #         break
+    #     if temp < -273.15:
+    #         print("Uneta temperatura je manja od apsolutne nule.")
+    #         break
+    #     print("Temperatura u farenhajtima iznosi: ", 9/5 * temp + 32)
+
+# -Napisati program koji ispisuej Fibonacijev niz
+
+    # prviBroj = 1
+    # drugiBroj = 1
+
+    # print(prviBroj)
+
+    # for i in range(10):
+    #     rezultat = prviBroj + drugiBroj
+    #     prviBroj = drugiBroj
+    #     drugiBroj = rezultat
+    #     print(prviBroj)
+
+# -Napisati program koji ispisuje slovo po slovo uneti string
+
+# tekst = 'Ovo je uneti tekst'
+
+# for i in tekst:
+#     print(i, end=' ')
+
+#IMPORTI
+
+# from math import pow
+# from math import *
+# from random import randint
+
+# #NEGACIJA
+
+# if not (10):
+#     print('sth')
+
+# #GRANANJE
+
+# if 'uslov':
+#     print()
+# elif 'nekiNoviUslov':
+#     print()
+# else:
+#     print()
