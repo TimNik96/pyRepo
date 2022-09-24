b60f35fb5e3d50e64fee84a9e699052406b36a14
ntim <timotic.nikola.27@gmail.com>
2022-09-24 14:59:12 +0200

    add if statements

--- cas_1.py
+++ cas_1.py
@@ -1,17 +1,31 @@
 # - Napisati program koji ce za unos imena, prezimena i broja godina kreirati sledeci ispis:
 #   ime.prezime.godine@gmail.com
 
-# ime = input("Unesite ime: ")
-# prezime = input("Unesite prezime: ")
-# godine = input ("Unesite godine: ")
+    # ime = input("Unesite ime: ")
+    # prezime = input("Unesite prezime: ")
+    # godine = input ("Unesite godine: ")
 
-# print(ime + "." + prezime + "." + godine + "@gmail.com")
+    # print(ime + "." + prezime + "." + godine + "@gmail.com")
 
 # - Napisati program koji ce za unos imena i prezimena, kao i godina, kreirati sledeci ispis:
 #   Ja sam Nikola Timotic i imam 27 godina.
 
-ime = input("Unesite ime: ")
-prezime = input("Unesite prezime: ")
-godine = eval(input ("Unesite godine: "))
+    # ime = input("Unesite ime: ")
+    # prezime = input("Unesite prezime: ")
+    # godine = eval(input ("Unesite godine: "))
 
-print("Ja sam " + ime + " " + prezime + " i imam", godine, "godina.")
+    # print("Ja sam " + ime + " " + prezime + " i imam", godine, "godina.")
+
+# - Napisati program koji konvertuje stepene celzijusove skale u farenhajtovu skalu.
+#   9/5 * celzijusoviStepeni + 32
+
+    # celzijus = eval(input("Unesite stepene u Celzijusima: "))
+    # farenhajt = 9 / 5 * celzijus + 32
+    # print("Uneti stepeni u Farenhajtima su:", farenhajt)
+
+# - Napisati program koji konkatenira dva stringa
+
+    # str_1 = "Pozdrav"
+    # str_2 = "svete!"
+
+    # print(str_1, str_2, sep=" ")
---
+++ cas_2.py
@@ -0,0 +1,8 @@
+# - Napisati program koji proverava da li je broj paran.
+
+# broj = eval(input("Unesite broj: "))
+    
+# if broj % 2 == 0:
+#     print("Uneti broj je paran.")
+# else:
+#     print("Uneti broj nije paran.")
