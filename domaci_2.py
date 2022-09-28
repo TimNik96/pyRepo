5e191659d1ce480c3c397303c8f972fa0ea84a07
ntimotic <timotic.nikola.27@gmail.com>
2022-09-29 01:40:41 +0200

    finish domaci_2

---
+++ domaci_2.py
@@ -0,0 +1,17 @@
+# - Napisati program koji za unetu uciteljicu ispisuje njene ucenike. (Indeks uciteljice u listi odgovara indeksu njenih ucenika)
+
+    # lista_uciteljica = ['Ljilja', 'Dragana', 'Gordana']
+    # lista_odeljenja = [['Petar', 'Milovan', 'Milos'], ['Nikola', 'Stefan', 'Strahinja'], ['Slavisa', 'Mirko', 'Nemanja']]
+
+    # for u in lista_uciteljica:
+    #     print(u)
+
+    # uciteljica = input("Izaberite jednu od tri uciteljice: ")
+    # lista_ucenika = []
+
+    # for odeljenje in lista_odeljenja:
+    #     if lista_odeljenja.index(odeljenje) == lista_uciteljica.index(uciteljica):
+    #         lista_ucenika = odeljenje
+            
+    # for ucenici in lista_ucenika:
+    #     print(ucenici)
